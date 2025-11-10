933a3e32255b1ec4c8c2e5e9096bad1d4bbacfd9
Jessie <JC2015080419@gmail.com>
2025-11-10 22:03:04 +0800

    homePage redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Page() {
-  // 真·白板：你也可以直接 return null
-  return (
-    <main className="p-6">
-      {/* 空白区域，后续随便填 */}
-    </main>
-  );
+  redirect('/todo');           
 }
